6d5aabd50a43566210eb918dbd51aa09bf3943dd
Jacek Kosiński <kashiash@gmail.com>
2023-07-13 21:05:37 +0200

    Add project files.

---
+++ MyXafSolution.Blazor.Server/Startup.cs
@@ -0,0 +1,122 @@
+﻿using DevExpress.ExpressApp.ApplicationBuilder;
+using DevExpress.ExpressApp.Blazor.ApplicationBuilder;
+using DevExpress.ExpressApp.Blazor.Services;
+using DevExpress.Persistent.Base;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components.Server.Circuits;
+using Microsoft.EntityFrameworkCore;
+using MyXafSolution.Blazor.Server.Services;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.OData;
+using DevExpress.ExpressApp.WebApi.Services;
+
+namespace MyXafSolution.Blazor.Server;
+
+public class Startup {
+    public Startup(IConfiguration configuration) {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services) {
+        services.AddSingleton(typeof(Microsoft.AspNetCore.SignalR.HubConnectionHandler<>), typeof(ProxyHubConnectionHandler<>));
+
+        services.AddRazorPages();
+        services.AddServerSideBlazor();
+        services.AddHttpContextAccessor();
+        services.AddScoped<CircuitHandler, CircuitHandlerProxy>();
+        services.AddXaf(Configuration, builder => {
+            builder.UseApplication<MyXafSolutionBlazorApplication>();
+
+            builder.AddXafWebApi(webApiBuilder => {
+                webApiBuilder.ConfigureOptions(options => {
+                    // Make your business objects available in the Web API and generate the GET, POST, PUT, and DELETE HTTP methods for it.
+                    // options.BusinessObject<YourBusinessObject>();
+                });
+            });
+
+            builder.Modules
+                .AddConditionalAppearance()
+                .AddReports(options => {
+                    options.EnableInplaceReports = true;
+                    options.ReportDataType = typeof(DevExpress.Persistent.BaseImpl.EF.ReportDataV2);
+                    options.ReportStoreMode = DevExpress.ExpressApp.ReportsV2.ReportStoreModes.XML;
+                })
+                .AddValidation(options => {
+                    options.AllowValidationDetailsAccess = false;
+                })
+                .Add<MyXafSolution.Module.MyXafSolutionModule>()
+            	.Add<MyXafSolutionBlazorModule>();
+            builder.ObjectSpaceProviders
+                .AddEFCore(options => options.PreFetchReferenceProperties())
+                    .WithDbContext<MyXafSolution.Module.BusinessObjects.MyXafSolutionEFCoreDbContext>((serviceProvider, options) => {
+                        // Uncomment this code to use an in-memory database. This database is recreated each time the server starts. With the in-memory database, you don't need to make a migration when the data model is changed.
+                        // Do not use this code in production environment to avoid data loss.
+                        // We recommend that you refer to the following help topic before you use an in-memory database: https://docs.microsoft.com/en-us/ef/core/testing/in-memory
+                        //options.UseInMemoryDatabase("InMemory");
+                        string connectionString = null;
+                        if(Configuration.GetConnectionString("ConnectionString") != null) {
+                            connectionString = Configuration.GetConnectionString("ConnectionString");
+                        }
+#if EASYTEST
+                        if(Configuration.GetConnectionString("EasyTestConnectionString") != null) {
+                            connectionString = Configuration.GetConnectionString("EasyTestConnectionString");
+                        }
+#endif
+                        ArgumentNullException.ThrowIfNull(connectionString);
+                        options.UseSqlServer(connectionString);
+                        options.UseChangeTrackingProxies();
+                        options.UseObjectSpaceLinkProxies();
+                        options.UseLazyLoadingProxies();
+                    })
+                .AddNonPersistent();
+        });
+
+        services
+            .AddControllers()
+            .AddOData((options, serviceProvider) => {
+                options
+                    .AddRouteComponents("api/odata", new EdmModelBuilder(serviceProvider).GetEdmModel())
+                    .EnableQueryFeatures(100);
+            });
+
+        services.AddSwaggerGen(c => {
+            c.EnableAnnotations();
+            c.SwaggerDoc("v1", new OpenApiInfo {
+                Title = "MyXafSolution API",
+                Version = "v1",
+                Description = @"Use AddXafWebApi(options) in the MyXafSolution.Blazor.Server\Startup.cs file to make Business Objects available in the Web API."
+            });
+        });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+        if(env.IsDevelopment()) {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyXafSolution WebApi v1");
+            });
+        }
+        else {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. To change this for production scenarios, see: https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+        app.UseHttpsRedirection();
+        app.UseRequestLocalization();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseXaf();
+        app.UseEndpoints(endpoints => {
+            endpoints.MapXafEndpoints();
+            endpoints.MapBlazorHub();
+            endpoints.MapFallbackToPage("/_Host");
+            endpoints.MapControllers();
+        });
+    }
+}
---
+++ MyXafSolution.Module/BusinessObjects/MyXafSolutionDbContext.cs
@@ -0,0 +1,45 @@
+﻿using DevExpress.ExpressApp.EFCore.Updating;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.ExpressApp.Design;
+using DevExpress.ExpressApp.EFCore.DesignTime;
+
+namespace MyXafSolution.Module.BusinessObjects;
+
+// This code allows our Model Editor to get relevant EF Core metadata at design time.
+// For details, please refer to https://supportcenter.devexpress.com/ticket/details/t933891.
+public class MyXafSolutionContextInitializer : DbContextTypesInfoInitializerBase {
+	protected override DbContext CreateDbContext() {
+		var optionsBuilder = new DbContextOptionsBuilder<MyXafSolutionEFCoreDbContext>()
+            .UseSqlServer(";")
+            .UseChangeTrackingProxies()
+            .UseObjectSpaceLinkProxies();
+        return new MyXafSolutionEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+//This factory creates DbContext for design-time services. For example, it is required for database migration.
+public class MyXafSolutionDesignTimeDbContextFactory : IDesignTimeDbContextFactory<MyXafSolutionEFCoreDbContext> {
+	public MyXafSolutionEFCoreDbContext CreateDbContext(string[] args) {
+		throw new InvalidOperationException("Make sure that the database connection string and connection provider are correct. After that, uncomment the code below and remove this exception.");
+		//var optionsBuilder = new DbContextOptionsBuilder<MyXafSolutionEFCoreDbContext>();
+		//optionsBuilder.UseSqlServer("Integrated Security=SSPI;Data Source=(localdb)\\mssqllocaldb;Initial Catalog=MyXafSolution");
+        //optionsBuilder.UseChangeTrackingProxies();
+        //optionsBuilder.UseObjectSpaceLinkProxies();
+		//return new MyXafSolutionEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+[TypesInfoInitializer(typeof(MyXafSolutionContextInitializer))]
+public class MyXafSolutionEFCoreDbContext : DbContext {
+	public MyXafSolutionEFCoreDbContext(DbContextOptions<MyXafSolutionEFCoreDbContext> options) : base(options) {
+	}
+	//public DbSet<ModuleInfo> ModulesInfo { get; set; }
+	public DbSet<ReportDataV2> ReportDataV2 { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotificationsWithOriginalValues);
+        modelBuilder.UsePropertyAccessMode(PropertyAccessMode.PreferFieldDuringConstruction);
+    }
+}
